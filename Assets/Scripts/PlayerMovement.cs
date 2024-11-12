9e820cf8afa24fdd3570f1c99f9b615954773b70
mikloskristian <kristian.miklos032@gmail.com>
2024-11-12 23:07:10 +0100

    Added Climbing, Broken.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float playerSpeed = 100.0f;
-    [SerializeField] private float playerJumpAmmount = 150.0f;
+    [SerializeField] private float playerSpeed = 10.0f;
+    [SerializeField] private float playerJumpAmmount = 15.0f;
+    [SerializeField] private float climbingSpeed = 10f;
     private Rigidbody2D playerRigidBody;
     private Animator playerAnimator;
     private BoxCollider2D boxCollider;
     private PolygonCollider2D polygonCollider;
     private float horizontalMovement;
+    private float verticalMovement;
     private bool isRunning;
     private bool isJumping;
+    private bool isClimbing;
     void Start()
     {
         playerRigidBody = GetComponent<Rigidbody2D>();
@@ -27,6 +31,7 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         Run();
+        Climb();
         if(polygonCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
         {
             Jump();
@@ -39,14 +44,35 @@ public class PlayerMovement : MonoBehaviour
 
         if(isJumping)
         {
-            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, playerJumpAmmount);
+            playerRigidBody.velocity = new UnityEngine.Vector2(playerRigidBody.velocity.x, playerJumpAmmount);
         }
+    }
+
+    private void Climb()
+    {
+        if(boxCollider.IsTouchingLayers(LayerMask.GetMask("Climbable")))
+        {
+            verticalMovement = Input.GetAxis("Vertical");
+            UnityEngine.Vector2 climbingVelocity = new UnityEngine.Vector2(playerRigidBody.velocity.x, verticalMovement * climbingSpeed);
+
+            playerRigidBody.velocity = climbingVelocity;
+
+            playerAnimator.SetBool("isClimbing", true);
+        }
+
+        playerAnimator.SetBool("isClimbing", false);
+    }
+
+    private void ChangeClimbingAnimation()
+    {
+        isClimbing = Mathf.Abs(playerRigidBody.velocity.y) > Mathf.Epsilon;
+        playerAnimator.SetBool("isClimbing", isClimbing);
     }
 
     private void Run()
     {
         horizontalMovement = Input.GetAxis("Horizontal");
-        playerRigidBody.velocity = new Vector2(horizontalMovement * playerSpeed, playerRigidBody.velocity.y);
+        playerRigidBody.velocity = new UnityEngine.Vector2(horizontalMovement * playerSpeed, playerRigidBody.velocity.y);
         FlipSprite();
         ChangeRunningAnimation();
     }
@@ -56,7 +82,7 @@ public class PlayerMovement : MonoBehaviour
         isRunning = Mathf.Abs(playerRigidBody.velocity.x) > Mathf.Epsilon;
         if(isRunning)
         {
-            transform.localScale = new Vector2(Mathf.Sign(playerRigidBody.velocity.x), transform.localScale.y);
+            transform.localScale = new UnityEngine.Vector2(Mathf.Sign(playerRigidBody.velocity.x), transform.localScale.y);
         }
     }
 
